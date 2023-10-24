2cc8dbbb7395925970650370975a19b738086b67
aqilaazza <aqilanurazza123@gmail.com>
2023-10-24 16:18:04 +0700

    update

--- Praktikum9.1/ArrayRataNilai05.java
+++ Praktikum9.1/ArrayRataNilai05.java
@@ -1,11 +1,17 @@
 import java.util.Scanner;
 
 public class ArrayRataNilai05 {
+    /**
+     * @param args
+     */
     public static void main(String[]args) {
         Scanner sc = new Scanner(System.in);
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+        double rata2, total = 0, ratalulus = 0, ratatdklulus = 0, totlulus= 0, tottdklulus = 0;
+        int jmllulus = 0, jmltdklulus = 0, jmlMhs; 
+
+        System.out.print(" Masukkan jumlah Mahasiswa : ");
+        jmlMhs = sc.nextInt();
+        int[] nilaiMhs = new int[jmlMhs];
 
         for (int i = 0; i < nilaiMhs.length; i++ ) {
             System.out.print("Masukkan nilai mahasiswa ke- " +(i+1)+ " : ");
@@ -14,9 +20,27 @@ public class ArrayRataNilai05 {
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
         }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = " +rata2);
 
-        
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            if(nilaiMhs[i] >70) {
+               // System.out.println("Mahasiswa ke- " + i + " lulus!");
+                jmllulus++;
+                totlulus += nilaiMhs[i];
+            } else {
+               // System.out.println("Mahasiswa ke- " + i + " tidak lulus!");
+                jmltdklulus++;
+                tottdklulus += nilaiMhs[i];
+            }
+              
+        }
+         //rata2 = total/nilaiMhs.length;
+         ratalulus = totlulus/jmllulus;
+         ratatdklulus = tottdklulus/jmltdklulus;
+           // System.out.println("Jumlah mahasiswa yang lulus : " + jmllulus);
+          //  System.out.println("Jumlah mahasiswa yang tidak lulus : " + jmltdklulus );
+          //  System.out.println("Nilai rata-rata : " + rata2);
+          System.out.println("Rata-rata nilai lulus : " +ratalulus);
+          System.out.println("Rata-rata nilai tidak lulus : " +ratatdklulus);
     }
+}
 
