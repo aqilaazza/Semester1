8699a0bc81e0dcc69a1132cea76ad76dd9dc12aa
aqilaazza <aqilanurazza123@gmail.com>
2023-10-31 17:26:05 +0700

    update

--- Praktikum10/BioskopWithScanner05.java
+++ Praktikum10/BioskopWithScanner05.java
@@ -3,28 +3,74 @@ public class BioskopWithScanner05 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int baris, kolom;
-        String nama, next;
+        int baris, kolom, umur, menu;
+        String nama, next, exit;
+        long noTlp;
 
         String[][] penonton = new String[4][2];
 
         while (true) {
+            System.out.println("Menu : ");
+            System.out.println("1. Masukkan data penonton");
+            System.out.println("2. Tampilkan data penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih Menu : ");
+            menu = sc.nextInt();
+            sc.nextLine();
+
+        switch (menu) {
+            case 1 : 
+            //input data penonton
             System.out.print("Masukkan nama : ");
             nama = sc.nextLine();
+            System.out.print("Masukkan umur : ");
+            umur = sc.nextInt();
+            System.out.print("Masukkan nomer telphone : ");
+            noTlp = sc.nextLong();
+
             System.out.print("Masukkan baris : ");
             baris = sc.nextInt();
             System.out.print("Masukkan kolom : ");
             kolom = sc.nextInt();
             sc.nextLine();
 
-            penonton [baris-1] [kolom-1] = nama;
-
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next = sc.nextLine();
-
-            if(next.equalsIgnoreCase("n")) {
-                break;
+            if (baris < 1 || baris > penonton.length || kolom < 1 || kolom > penonton[0].length) {
+                System.out.println("Nomor baris/kolom kursi tidak tersedia.");
+                System.out.println("Coba lagi.");
+                continue;
             }
-        }
+
+            if (penonton[baris - 1][kolom - 1] != null) {
+                System.out.println("Kursi sudah terisi.");
+                System.out.println("Coba lagi ");
+                System.out.print("Masukkan baris : ");
+                baris = sc.nextInt();
+                System.out.print("Masukkan kolom : ");
+                kolom = sc.nextInt();
+            }
+
+            penonton [baris-1] [kolom-1] = nama;
+            break;
+    
+
+            case 2 :
+            for (int i = 0; i < penonton.length; i++) {
+                for (int a = 0; a < penonton[i].length; a++) {
+                if (penonton[i][a] != null) {
+                    System.out.println("Penonton berada pada baris " + (i+1) + ", dan kolom " + (a + i) + " : " + penonton [i][a]);
+             }  else {
+                System.out.println("Penonton berada pada baris " + (i+1) + ", dan kolom " + (a+i)+ " : *** " );
+             }
+         }
+     }
+     break;
+     case 3 :
+         System.exit(0);
+     default :
+         System.out.println("Menu tidak valid");
+         System.out.println("Coba lagi");
+
+    }
+}
     }
 }
--- Praktikum10/Numbers05.java
+++ Praktikum10/Numbers05.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 public class Numbers05 {
     public static void main(String[] args) {
@@ -7,5 +8,16 @@ public class Numbers05 {
         myNumbers [0] = new int[5];
         myNumbers [1] = new int[3];
         myNumbers [2] = new int[1];
+
+        //inisialisasi myNumbers
+        for (int i = 0; i < myNumbers.length; i++) {
+            for (int j = 0; j < myNumbers[i].length; j++) {
+                myNumbers[i][j] = 0;
+            }
+        }
+    
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
     }
 }
