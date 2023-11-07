74c793a96b57c1636af54a89bb3084baad3c428a
aqilaazza <aqilanurazza123@gmail.com>
2023-11-07 18:23:48 +0700

    update

--- Praktikum11/NestedLoop_2341760022.java
+++ Praktikum11/NestedLoop_2341760022.java
@@ -1,30 +1,22 @@
 import java.util.Scanner;
 public class NestedLoop_2341760022 {
-    private static int i;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int [][] temps = new int[5][7];
+        double [][] temps = new double[5][7];
 
         for(int i = 0; i < temps.length; i++) {
             System.out.println("Kota ke-" + i);
             for (int j = 0; j < temps[0].length; j++) {
                 System.out.print("Hari ke-" + (j + 1) + ": ");
-                temps[i][j] = sc.nextInt();
+                temps[i][j] = sc.nextDouble();
             }
             System.out.println();
         }
 
-       //for (int i = 0; i < temps.length; i++) {
-            //System.out.print("Kota ke-" + (i + 1) + ": ");
-           //for(int j = 0; j < temps[0].length; j++){
-             //  System.out.print(temps[i][j] + " ");
-           //}
-           //System.out.println();
-       //}
-       for (int [] temp : temps) {
-            System.out.print("Kota ke-" + (i + 0) + ": ");
-            for (int temperature : temp) {
+       for (int i = 0; i < temps.length; i++ ) {
+            System.out.print("Kota ke-" + (i + 1) + ": ");
+            for (double temperature : temps[i]) {
                 System.out.print(temperature + " ");
             }
             System.out.println();
@@ -32,7 +24,7 @@ public class NestedLoop_2341760022 {
        // rata rata setiap kota
        for (int i = 0; i < temps.length; i++) {
         double rata = 0;
-        for (int temperature : temps[i]) {
+        for (double temperature : temps[i]) {
             rata += temperature;
         }
         rata /= temps[i].length;
