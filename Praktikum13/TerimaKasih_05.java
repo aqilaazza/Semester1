31209e5e95bf8546bf951d3966cbe79666ac7931
aqilaazza <aqilanurazza123@gmail.com>
2023-11-21 09:59:33 +0700

    update

--- Praktikum13/TerimaKasih_05.java
+++ Praktikum13/TerimaKasih_05.java
@@ -1,18 +1,18 @@
 //Membuat Fungsi TANPA Parameter
 
-import java.util.Scanner;
+//import java.util.Scanner;
 public class TerimaKasih_05 {
     /**
      * @param args
      */
 
-        
-        public static void UcapanTerimaKasih() {
+        //Tanpa Fungsi
+        public static void main(String[] args) {
             System.out.println("Thank you for being the best teacher in the world.\n" + 
             "You inspired in me a love for learning and made me fell like I could ask you anything.");
         }
 
-        public static void main (String[] args ) {
-            UcapanTerimaKasih();
-        }
+        //public static void main (String[] args ) {
+            //UcapanTerimaKasih();
+       // }
     }
