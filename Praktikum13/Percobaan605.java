c384aaf1de755587ee1066039cd826f1db622bb1
aqilaazza <aqilanurazza123@gmail.com>
2023-11-22 00:06:29 +0700

    update

--- Praktikum13/Percobaan605.java
+++ Praktikum13/Percobaan605.java
@@ -1,52 +1,35 @@
 //Pembuatan kode program, dengan fungsi versus tanpa fungsi
 
 import java.util.Scanner;
+
 public class Percobaan605 {
-
-     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        int p,l,t,L,vol;
-
-        System.out.print("Masukkan panjang : ");
-        p = input.nextInt();
-        System.out.println("Masukkan lebar : ");
-        l = input.nextInt();
-        System.out.println("Masukkan tinggi : ");
-        t = input.nextInt();
-
-        L = p * l;
-        System.out.println("Luas persegi panjang adalah : " +L);
-
-        vol = p * l * t;
-        System.out.println("Volume balok adalah : " +vol);
+        static int hitungLuas (int pjg, int lb) {
+            int Luas = pjg * lb;
+            return Luas;
+        }
+        //Fungsi hitungVolume
+     static int hitungVolume (int tinggi, int a, int b) {
+        int volume = hitungLuas (a,b) * tinggi;
+        return volume;
      }
-
-     //Fungsi hitungLuas
-     static int hitungVolume (int t, int a, int b) {
-        int vol = hitungLuas (a,b) * t;
-        return vol;
-     }
-
-     private static int hitungLuas(int a, int b) {
-        return 0;
-    }
 
     //Fungsi main
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in); 
         int p,l,t,L, vol;
-        System.out.println("Masukkan panjang : ");
+        System.out.print("Masukkan panjang : ");
         p = input.nextInt();
-        System.out.println("masukkan lebar : ");
+        System.out.print("masukkan lebar : ");
         l = input.nextInt();
-        System.out.println("masukkan tinggi : ");
+        System.out.print("masukkan tinggi : ");
         t = input.nextInt();
 
         L = hitungLuas(p,l);
         System.out.println("Luas Persegi Panjang adalah " +L);
         vol = hitungVolume(t,p,l);
         System.out.println("Volume balok adalah " +vol);
-     }
-}
+    
+    }
+    }
+
 
--- Praktikum13/UcapanTerimaKasih_05.java
+++ Praktikum13/UcapanTerimaKasih_05.java
@@ -1,8 +1,9 @@
 //Fungsi yang dapat meng-Calling Fugsi lain
 
 import java.util.Scanner;
+
 public class UcapanTerimaKasih_05 {
-    
+
     public static String PenerimaUcapan() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Tuliskan NAMA orang yang ingin Anda beri ucapan : ");
@@ -11,13 +12,21 @@ public class UcapanTerimaKasih_05 {
         return namaOrang;
     }
 
-     public static void UcapanTerimaKasih(){
-            String nama = PenerimaUcapan();
-            System.out.println("Thank you " +nama+ " for being the best teacher in the world\n" +
-            "You inspired in me a love for learning and made me feel like I could ask you anything");
-        }
+    public static void UcapanTerimaKasih() {
+        System.out.println("Thank you " + PenerimaUcapan() +
+                " for being the best teacher in the world\n" +
+                "You inspired in me a love for learning and made me feel like I could ask you anything");
+    }
 
-    public static void main(String[]args) {
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
+    }
+
+    public static void main(String[] args) {
         UcapanTerimaKasih();
+        //memanggil ucapan tambahan
+        String ucapan = "Hope you alwaays be happy in your live";
+        UcapanTambahan(ucapan);
     }
 }
+
