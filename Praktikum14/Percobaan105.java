2b6bbaf5c4dd048e98ec24ff8fa0e9a24fac1aad
aqilaazza <aqilanurazza123@gmail.com>
2023-11-28 08:39:03 +0700

    commit

---
+++ Praktikum14/Percobaan105.java
@@ -0,0 +1,21 @@
+public class Percobaan105 {
+
+   static int faktorialRekursif(int n) {
+    if(n == 0) {
+        return (1);
+    } else {
+        return (n * faktorialRekursif(n-1));
+    }
+   }
+   static int faktorialIteraktif(int n) {
+    int faktor= 1;
+    for (int i = n; i >= 1; i--) {
+        faktor = faktor * i;
+    }
+    return faktor;
+   }
+   public static void main(String[] args) {
+    System.out.println(faktorialRekursif(5));
+    System.out.println(faktorialIteraktif(5));
+   }
+}
